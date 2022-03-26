3b4e9d775fc02ed87fbb2d96b4f093c98d09f999
Samuel Wilson <samWson@users.noreply.github.com>
2022-03-26 20:22:06 +1300

    Print the character code of entered keys

--- lib/corundum/editor.rb
+++ lib/corundum/editor.rb
@@ -2,13 +2,20 @@ require 'io/console'
 
 module Corundum
   class Editor
+    ASCII_CONTROL_CODES = 0..31
+
     def main
       STDIN.raw!
 
       ch = nil
-      while ch != 'q'
-        ch = STDIN.getch()
-        puts ch
+      while ch != 'q'.ord
+        ch = STDIN.getch().ord
+
+        if ASCII_CONTROL_CODES.include?(ch)
+          printf("%d\n", ch)
+        else
+          printf("%d ('%c')\n", ch, ch)
+        end
       end
 
       STDIN.cooked!
