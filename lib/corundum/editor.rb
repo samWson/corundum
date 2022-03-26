e2adb5d474326d3fd5d8f3429583df076bc18a22
Samuel Wilson <samWson@users.noreply.github.com>
2022-03-26 20:59:17 +1300

    Exit program with `ctrl + q`
    `ctrl + q` replaces the program exit character `q`. The `control_key`
    method does a bitwise AND operation with the value 00011111. It sets the
    upper three bytes to 0. This mimics what the control key does.

--- lib/corundum/editor.rb
+++ lib/corundum/editor.rb
@@ -3,12 +3,13 @@ require 'io/console'
 module Corundum
   class Editor
     ASCII_CONTROL_CODES = 0..31
+    ASCII_CONTROL_KEY = 0x1f
 
     def main
       STDIN.raw!
 
-      ch = nil
-      while ch != 'q'.ord
+      while true
+        ch = nil
         ch = STDIN.getch().ord
 
         if ASCII_CONTROL_CODES.include?(ch)
@@ -16,11 +17,19 @@ module Corundum
         else
           printf("%d ('%c')\r\n", ch, ch)
         end
+
+        break if ch == control_key('q')
       end
 
       STDIN.cooked!
 
       exit(0)
+    end
+
+    private
+
+    def control_key(key)
+      key.ord & ASCII_CONTROL_KEY
     end
   end
 end
