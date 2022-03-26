8ae3975f456f8542a33dcd7af9705158ee56f836
Samuel Wilson <samWson@users.noreply.github.com>
2022-03-26 19:55:31 +1300

    Enter and exit terminal raw mode
    There are some default params that haven't been set for `raw!`. Because
    of this only a single byte will be read when a read operation is
    performed. This suits me for now.
    
    `getch` reads and returns a character in raw mode. It has the same
    params and defaults as `raw!`.
    
    At the end I presume `cooked!` returns the terminal to normal mode give
    cooked is the opposite of raw.
    
    Strictly speaking manually setting to raw mode and going back to cooked
    isn't required since `getch` also reads in raw mode. For now I'll leave
    it this way since it is quite clear what mode we are in and there may
    be other operations that are added which I want to be in raw mode for.

--- lib/corundum/editor.rb
+++ lib/corundum/editor.rb
@@ -3,11 +3,15 @@ require 'io/console'
 module Corundum
   class Editor
     def main
+      STDIN.raw!
+
       ch = nil
       while ch != 'q'
         ch = STDIN.getch()
         puts ch
       end
+
+      STDIN.cooked!
 
       exit(0)
     end
